dbb12384ea2fa5f9f8cf3513224eeaee82f9f8ba
jiro4989 <jiroron666@gmail.com>
2018-11-15 02:10:48 +0900

    [update]upgrade kibit version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject meme "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :plugins [[lein-kibit "0.0.8"]]
+  :plugins [[lein-kibit "0.1.6"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
