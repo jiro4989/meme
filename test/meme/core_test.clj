d0e60e4f7229b7dc86bdd70e3b9a8eb58db8f8aa
jiro4989 <jiroron666@gmail.com>
2018-11-17 18:04:42 +0900

    [fix]期待値誤りを修正

--- test/meme/core_test.clj
+++ test/meme/core_test.clj
@@ -33,10 +33,10 @@
 (deftest test-format-line
   (testing "format-lineは"
     (testing "通常出力"
-      (is (= " 1:cat" (format-line m {:none false :padding-size 3 :delimiter \:}))))
+      (is (= "  1:cat" (format-line m {:none false :padding-size 3 :delimiter \:}))))
     (testing "none指定ありの場合は数値を返さない"
       (is (= "cat" (format-line m {:none true :padding-size 3 :delimiter \:}))))
     (testing "区切り文字を指定できる"
-      (is (= " 1,cat" (format-line m {:none false :padding-size 3 :delimiter ","}))))
+      (is (= "  1,cat" (format-line m {:none false :padding-size 3 :delimiter ","}))))
     ))
 
