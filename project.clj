532cffddbd31d07f1489d500e09b2f7877ab4cc0
jiro4989 <jiroron666@gmail.com>
2018-11-17 19:49:27 +0900

    [update]ベンチマークライブラリ追加

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.cli "0.4.1"]]
+                 [org.clojure/tools.cli "0.4.1"]
+                 [criterium "0.4.4"]]
   :profiles {:uberjar {:main meme.core :aot :all}}
   :main meme.core)
