64b34908103334a0bfbd0ff7f69e42b7a8d485bd
jiro4989 <jiroron666@gmail.com>
2018-11-17 18:01:06 +0900

    [update]出力書式関数のテストコード追加

--- test/meme/core_test.clj
+++ test/meme/core_test.clj
@@ -28,3 +28,15 @@
       (is (= ["cat" "cc" "con"] (command-names ["cat" "concat"] 1 ["cat" "con"]))))
     ))
 
+(def m {:weight 1 :name "cat"})
+
+(deftest test-format-line
+  (testing "format-lineは"
+    (testing "通常出力"
+      (is (= " 1:cat" (format-line m {:none false :padding-size 3 :delimiter \:}))))
+    (testing "none指定ありの場合は数値を返さない"
+      (is (= "cat" (format-line m {:none true :padding-size 3 :delimiter \:}))))
+    (testing "区切り文字を指定できる"
+      (is (= " 1,cat" (format-line m {:none false :padding-size 3 :delimiter ","}))))
+    ))
+
