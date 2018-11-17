11f03a3c8456275a58da824091f232fe14f20b6c
jiro4989 <jiroron666@gmail.com>
2018-11-17 11:02:56 +0900

    [update]分離した関数を削除

--- src/meme/core.clj
+++ src/meme/core.clj
@@ -3,9 +3,6 @@
             [clojure.java.io :as io])
   (:gen-class))
 
-; 母音
-(def vowel "aiueo")
-
 (defn takes
   "文字列から文字列を指定数まで順に取り出して返す
   ex: (takes 2 \"xyz\")
@@ -44,50 +41,6 @@
        flatten
        distinct
        sort))
-
-(defn weight-contain-word
-  [word common-words]
-  (if (some #(= % word) common-words)
-    5
-    0))
-
-(defn weight-vowel
-  [word]
-  (if (empty? word)
-    0
-    (let [c (->> word
-                 (filter #(some (fn [x] (= x %)) vowel))
-                 count)
-          r (-> c 
-                (/ (count word))
-                float)]
-      (cond
-        (< 0.75 r) 3
-        (< 0.5 r) 2
-        (< 0.25 r) 1
-        :else 0))))
-
-(defn weight-short-consonant
-  [word]
-  (if (or (empty? word)
-          (not (zero? (->> word
-                           (filter #(some (fn [x] (= x %)) vowel))
-                           count))))
-    0
-    (let [c (count word)]
-      (cond
-        (< c 2) 0
-        (< c 3) 2
-        (< c 4) 1
-        (< c 5) 0
-        :else 0))))
-
-(defn weight
-  [word common-words]
-  (let [w (+ (weight-contain-word word common-words)
-             (weight-vowel word)
-             (weight-short-consonant word))]
-    {:weight w :word word}))
 
 (defn -main
   [& args]
--- test/meme/core_test.clj
+++ test/meme/core_test.clj
@@ -22,58 +22,6 @@
       (is (= ["con" "cat" "c"] (include-common-words "concat" ["con" "cat" "x" "c"]))))
     ))
 
-(deftest test-weight-contain-word
-  (testing "weight-contain-wordは"
-    (testing "単語が一般用語に含まれるなら5を返す"
-      (is (= 5 (weight-contain-word "cat" ["concat" "cat"]))))
-    (testing "単語が一般用語に含まれないなら0を返す"
-      (is (= 0 (weight-contain-word "cat" ["concat"]))))
-    (testing "単語が一般用語に含まれないなら0を返す"
-      (is (= 0 (weight-contain-word "cat" []))))
-    ))
-
-(deftest test-weight-vowel
-  (testing "weight-vowelは"
-    (testing "母音が75%含まれるなら3"
-      (is (= 3 (weight-vowel "aiueo"))))
-    (testing "母音が50%含まれるなら2"
-      (is (= 2 (weight-vowel "caaat"))))
-    (testing "母音が25%含まれるなら1"
-      (is (= 1 (weight-vowel "caat"))))
-    (testing "母音が含まれなければ0"
-      (is (= 0 (weight-vowel "ct"))))
-    (testing "母音が含まれなければ0"
-      (is (= 0 (weight-vowel ""))))
-    (testing "母音が含まれなければ0"
-      (is (= 0 (weight-vowel nil))))
-    ))
-
-(deftest test-weight-short-consonant
-  (testing "weight-short-consonantは"
-    (testing "子音が1文字なら0"
-      (is (= 0 (weight-short-consonant "x"))))
-    (testing "子音が2文字なら2"
-      (is (= 2 (weight-short-consonant "xy"))))
-    (testing "子音が3文字なら1"
-      (is (= 1 (weight-short-consonant "xyz"))))
-    (testing "子音が4文字以上なら0"
-      (is (= 0 (weight-short-consonant "xyzx"))))
-    (testing "子音が4文字以上なら0"
-      (is (= 0 (weight-short-consonant "xyzxy"))))
-    (testing "空文字なら0"
-      (is (= 0 (weight-short-consonant ""))))
-    (testing "空文字なら0"
-      (is (= 0 (weight-short-consonant nil))))
-    (testing "母音が含まれていたら0"
-      (is (= 0 (weight-short-consonant "xyaz"))))
-    ))
-
-; (deftest test-weight
-;   (testing "weightは"
-;     (testing "TODO"
-;       (is (= 0 (weight "cat" ["concat" "cat"]))))
-;     ))
-
 (deftest test-command-names
   (testing "command-namesは"
     (testing "コマンド名を返す"
